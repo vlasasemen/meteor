091903a629d6c172b878d7ed970585d49afe6110
222Alex2222 <106630660+222Alex2222@users.noreply.github.com>
2025-04-14 05:20:31 +0300

    Добавление адаптации для телефонов

--- public/java.js
+++ public/java.js
@@ -1,21 +1,12 @@
-// Проверяем, является ли устройство мобильным
-const isMobile =
-	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-		navigator.userAgent
-	)
+window.addEventListener('scroll', e => {
+	document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
+})
 
-if (!isMobile) {
-	// Параллакс эффекты только для десктопов
-	window.addEventListener('scroll', e => {
-		document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
-	})
-
-	gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-	ScrollSmoother.create({
-		wrapper: '.wrapper',
-		content: '.content',
-	})
-}
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content',
+})
 
 $(document).ready(function () {
 	$('.layer__header').css('opacity', '0')
@@ -27,15 +18,4 @@ $(document).ready(function () {
 		},
 		1500
 	)
-
-	// Для мобильных устройств отключаем сложные анимации
-	if (isMobile) {
-		$(
-			'.layer, .layer__header, .main-article__header, .main-article__paragraph'
-		).css({
-			transform: 'none',
-			transition: 'none',
-			'will-change': 'auto',
-		})
-	}
 })
